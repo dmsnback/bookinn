c1b650cf62d081749dd6b6fac7dd9bf308c19dbe
Dmitry Titenkov <lt200711@yandex.ru>
2025-11-18 21:59:03 +0300

    docker compose загружает образ из dokerhub

--- backend/bookinn/settings.py
+++ backend/bookinn/settings.py
@@ -65,6 +65,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
